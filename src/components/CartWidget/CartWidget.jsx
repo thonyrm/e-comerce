1e0f00bd86a8f2324ec3c12ad90b5c2032b15c39
Anthony Ramos Morales <100253179+thonyrm@users.noreply.github.com>
2026-02-12 06:03:12 -0300

    Move Link import to the top of CartWidget.jsx

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,7 +1,6 @@
-import { Link } from 'react-router-dom'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { CarritoContext } from '../../context/CarritoContext'
-
 import './CartWidget.css'
 
 const CartWidget = () => {
